ee0f0f9c909a99663b0725baa250eab24ab053e0
Stage777999 <ikingib1979@gmail.com>
2024-12-31 11:38:44 +0500

    finished

---
+++ main.js
@@ -0,0 +1,25 @@
+// 1. https://the-winter.github.io/codingjs/exercise.html?name=firstLast6&title=Array-1
+
+// function firstLast6(nums) {
+//     return nums[0] === 6 || nums[nums.length - 1] === 6;
+//   }
+
+
+// 2.https://the-winter.github.io/codingjs/exercise.html?name=sameFirstLast&title=Array-1   
+
+// function sameFirstLast(nums) {
+    // return nums.length >= 1 && nums[0] === nums[nums.length - 1];
+//  }
+
+// 3.https://the-winter.github.io/codingjs/exercise.html?name=commonEnd&title=Array-1
+
+// function commonEnd(a, b) {
+    // return a[0] === b[0] || a[a.length - 1] === b[b.length - 1];
+//   }
+  
+//  4.https://the-winter.github.io/codingjs/exercise.html?name=sum3&title=Array-1
+
+// function sum3(nums) {
+    // return nums[0] + nums[1] + nums[2];
+//   }
+  
